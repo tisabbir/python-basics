a30416c579cbed4fd8bfe3327f81ae08b0b4b6e2
TI Sabbir <tisabbir0@gmail.com>
2025-01-06 18:37:45 +0600

    add : number checker

---
+++ New Project/Condition/check_number.py
@@ -0,0 +1,7 @@
+number = int(input("Enter the number : "))
+if number > 0:
+    print("The number is positive")
+elif number == 0:
+    print("The number is Zero")
+else :
+    print("The number is negative")
