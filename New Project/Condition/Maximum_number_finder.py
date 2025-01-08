de27122d4ff0c453f8087410104e521e234dd1b8
TI Sabbir <tisabbir0@gmail.com>
2025-01-08 18:56:17 +0600

    add : maximum number finder

---
+++ New Project/Condition/Maximum_number_finder.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+c = int(input("Enter the third number : "))
+
+if a>b  and a >c :
+    print(f"The maximum number is first number = {a}")
+
+elif b>a and b>c :
+    print(f"The maximum number is second number = {b}")
+else: 
+    print(f"The maximum number is third number : {c}")
