41e8444488d2eb66b66e12d272ed969869bdd9f8
TI Sabbir <tisabbir0@gmail.com>
2025-01-09 19:11:33 +0600

    modify : for loop

--- New Project/Loops/for_loop.py
+++ New Project/Loops/for_loop.py
@@ -1,4 +1,7 @@
 fruits = ["apple", "banana", "orange", "grape", "jackfruit"]
 
-for fruit in fruits :
-    print(f"{fruit} is delicious")
+# for fruit in fruits :
+    # print(f"{fruit} is delicious")
+
+for i in range(100) :
+    print(i)
