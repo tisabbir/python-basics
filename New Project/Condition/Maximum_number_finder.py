1a351398bff4ae1b274076d3b84a76dd53e87968
TI Sabbir <tisabbir0@gmail.com>
2025-01-09 19:14:32 +0600

    modify : maximum number chcker

--- New Project/Condition/Maximum_number_finder.py
+++ New Project/Condition/Maximum_number_finder.py
@@ -5,7 +5,7 @@ c = int(input("Enter the third number : "))
 if a>b  and a >c :
     print(f"The maximum number is first number = {a}")
 
-elif b>a and b>c :
+elif b>c :
     print(f"The maximum number is second number = {b}")
 else: 
     print(f"The maximum number is third number : {c}")
