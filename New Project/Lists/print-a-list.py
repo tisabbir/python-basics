53061925a570677cb1902a0ad2073133a0195d40
TI Sabbir <tisabbir0@gmail.com>
2025-01-23 18:40:26 +0600

    add : list file

---
+++ New Project/Lists/print-a-list.py
@@ -0,0 +1,3 @@
+my_list = [1,2,3,4,1,2,3,4,5,6,3,4,5,6,7]
+for element in my_list:
+    print(element)
---
+++ New Project/Lists/store-name-&-display.py
@@ -0,0 +1,6 @@
+name_list=[]
+for i in range(7):
+    name_list.append(input("Enter names: "))
+
+for name in name_list:
+    print(name)
