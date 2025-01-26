72f9bb85103904713e40e1f8481082dc9341b191
TI Sabbir <tisabbir0@gmail.com>
2025-01-26 19:00:02 +0600

    add : tuple sum

--- New Project/tuple-set/tuple_sum.py
+++ New Project/tuple-set/tuple_sum.py
@@ -1,6 +1,10 @@
-numbers_tuple=(1,2,3,4,5,6,7,8,9)
-sum = 0
-for i in numbers_tuple:
-    sum = sum + i
+# sum = 0
+# for number in numbers_tuple:
+#     sum = sum + number
 
-print(sum)
+# print(sum)
+
+numbers_tuple=(1,2,3,4,5,6,7,8,9)
+print("jibito",numbers_tuple)
+numbers_set = set( numbers_tuple)
+print("kofine", numbers_set)
