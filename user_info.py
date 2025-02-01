bf4416ab8914a1f800f809b09423bda5431f46dd
TI Sabbir <tisabbir0@gmail.com>
2025-02-01 15:49:27 +0600

    add : user_info taker added

---
+++ user_info.py
@@ -0,0 +1,7 @@
+email = input("Enter your email: ")
+Name = input("Enter your Name: ")
+Fathers_Name = input("Enter your Fathers_Name: ")
+Mothers_Name = input("Enter your Mothers_Name: ")
+Institute_Name = input("Enter your Institute_Name: ")
+
+user_info = [email, Name, Fathers_Name, Mothers_Name, Institute_Name]
