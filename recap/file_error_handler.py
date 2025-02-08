60782ffb6d2da75c747bbdeafc5f10b10c58d494
TI Sabbir <tisabbir0@gmail.com>
2025-02-08 15:57:14 +0600

    add : file opener of error handler

---
+++ recap/file_error_handler.py
@@ -0,0 +1,8 @@
+try:
+    file = open("myfile.txt", "r")  
+    content = file.read()
+    print(content)
+    file.close()
+
+except FileNotFoundError:
+    print("❌ Error: The file does not exist. Please check the file name.")
